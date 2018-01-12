5cf35732c7dbe7a59385930c5f6c439ad6f18a99
kokke <spam@rowdy.dk>
2018-01-13 00:22:22 +0100

    Add files via upload

---
+++ tests/hand_picked.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "bn.h"
+
+
+int npassed = 0;
+int ntests = 0;
+
+
+static void test_evil(void)
+{
+  ntests += 1;
+  /*
+    Evil test case triggering infinite-loop - reported by jepler@github
+  */
+  {
+    struct bn a, b, c;
+    bignum_from_int(&a, 1); 
+    bignum_init(&b); bignum_dec(&b); // b now holds biggest bignum
+    bignum_div(&b, &a, &c);
+  }
+  /* test passed if it doesn't go into infinite-loop... */
+  npassed += 1;
+}
+
+
+
+int main()
+{
+  printf("\nRunning hand-picked test cases:\n");
+
+  test_evil();
+
+  printf("\n%d/%d tests successful.\n", npassed, ntests);
+  printf("\n");
+
+  return 0;
+}
+
